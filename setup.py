76a218cdce6730b7151e19d85445a3b799e6e9f8
Brant Faircloth <brant.faircloth@gmail.com>
2012-05-05 15:27:16 -0700

    attempt to fix setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-
 from distutils.core import setup
 
-setup(
-    name="mpi4py_map",
-    version="0.2",
-    author="Thomas V. Wiecki",
-    author_email="thomas.wiecki@gmail.com",
-    url="http://github.com/twiecki/mpi4py_map",
-    description="mpi4py_map provides a simple map() interface to mpi4py that allows easy parallelization of function evaluations over sequential input.",
-    install_requires=['mpi4py'],
-    setup_requires=['mpi4py'],
-    classifiers=[
-                'Development Status :: 3 - Alpha',
-                'Environment :: Console',
-                'Operating System :: OS Independent',
-                'Intended Audience :: Science/Research',
-                'License :: OSI Approved :: GNU General Public License (GPL)',
-                'Programming Language :: Python',
-                'Topic :: Scientific/Engineering',
-                 ]
-)
+if __name__ == '__main__':
+    setup(
+        name="mpimap",
+        version="0.2",
+        author="Thomas V. Wiecki",
+        author_email="thomas.wiecki@gmail.com",
+        url="http://github.com/twiecki/mpi4py_map",
+        description="mpi4py_map provides a simple map() interface to mpi4py that allows easy parallelization of function evaluations over sequential input.",
+        install_requires=['mpi4py'],
+        setup_requires=['mpi4py'],
+        classifiers=[
+                    'Development Status :: 3 - Alpha',
+                    'Environment :: Console',
+                    'Operating System :: OS Independent',
+                    'Intended Audience :: Science/Research',
+                    'License :: OSI Approved :: GNU General Public License (GPL)',
+                    'Programming Language :: Python',
+                    'Topic :: Scientific/Engineering',
+                     ],
+        packages=[
+                'mpimap'
+            ]
+    )
